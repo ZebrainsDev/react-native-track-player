b6ca09110fc39da74075e7687e94656c345d317d
Николай Вовк <n.vovk@zebrains.team>
2019-11-18 13:48:38 +0300

    Fix initQueue adding tracks

--- android/src/main/java/com/guichaguri/trackplayer/module/MusicModule.java
+++ android/src/main/java/com/guichaguri/trackplayer/module/MusicModule.java
@@ -240,21 +240,11 @@ public class MusicModule extends ReactContextBaseJavaModule implements ServiceCo
             }
 
 
-            int index = -1;
-            for(int i = 0; i < trackList.size(); i++) {
-                if(trackList.get(i).id.equals(headTrackId)) {
-                    index = i;
-                    break;
-                }
-            }
-         
-            if(index == -1) {
-                callback.reject("track_not_in_queue", "Given track ID was not found in queue");
-            } else if(trackList == null || trackList.isEmpty()) {
+            if(trackList == null || trackList.isEmpty()) {
                 callback.reject("invalid_track_object", "Track is missing a required key");
             } else {
                 binder.getPlayback().reset();
-                binder.getPlayback().add(trackList, index, callback);
+                binder.getPlayback().add(trackList, 0, callback);
                 binder.getPlayback().skip(headTrackId, callback);
             }
         });
